4aca3a6c53c1d04c2c5ec31aedd136dc1dba1c1a
eslam <eslam@DESKTOP-1PQV51B>
2021-08-19 23:13:14 +0200

    Add project files.

---
+++ ShopAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+ 
+        [HttpGet("getall")]
+        public IEnumerable<string> Get()
+        {
+
+            string[] customers = new string[] { "asdasd","asdasd"};
+
+            return customers;
+             
+        }
+    }
+}
---
+++ ShopAPI/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopAPI.Models
+{
+    public class Customers
+    {
+        public int customerNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string Phone { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public int SalesRepEmployeeNumber { get; set; }
+        public float CreditLimit { get; set; }
+
+
+    }
+}
