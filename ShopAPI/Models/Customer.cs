1215c09f3a03ceb7c5ab736803eb8572b3bc73e7
eslam <eslamelbazedy@gmail.com>
2021-08-20 07:47:10 +0200

    init commit

--- ShopAPI/Models/Customer.cs
+++ ShopAPI/Models/Customer.cs
@@ -19,8 +19,6 @@ namespace ShopAPI.Models
         public string State { get; set; }
         public string PostalCode { get; set; }
         public string Country { get; set; }
-
- 
         public float CreditLimit { get; set; }
 
 
